e5f14a13d9d56e49b515211d22e63703be2fc8e9
JuanSoulAxel <barruan.mm@gmail.com>
2023-04-08 13:28:31 -0300

    .

--- cadastrar.php
+++ cadastrar.php
@@ -5,15 +5,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Cadastar Evento</title>
 </head>
 <body>
 
 <form  action="controllers/ControllerCadEvents.php" method="POST" enctype="multipart/form-data">
-                <input class="" name="inpDisplay" type="text"> <br> <br>
-                <input name="inpColor" type="text"> <br> <br> 
-                <input name="inpStart" type="text"> <br> <br>
-                <input name="inpEnd" type="text">
+                <input class="" name="inpDisplay" type="text" required> <br> <br>
+                <select name="inpColor" required>
+                    <option value="red">Vermelho</option>
+                    <option value="blue">Azul</option>
+                </select>
+                 <br> <br> 
+                <input name="inpStart" type="date" min="2023-01-01" max="2023-12-31" required> <br> <br>
+                <input name="inpEnd" type="date" min="2023-01-01" max="2023-12-31" required>
                 <button type="submit">Save changes</button>
 </form>
 </body>
--- controllers/ControllerCadEvents.php
+++ controllers/ControllerCadEvents.php
@@ -7,6 +7,10 @@ $color = $_POST['inpColor'];
 $start = $_POST['inpStart'];
 $end = $_POST['inpEnd'];
 
+/*o fullcalender marca como se o end tivesse com um dia antes, fazendo a 
+função abaixo ele acrescenta um dia e marca o calendário corretamente.*/
+$end = date('Y-m-d', strtotime("+1 days",strtotime($end)));
+
 
 $mandarPraFuncao=new ClassEvents();
 $mandarPraFuncao->cadEvents($display, $color, $start, $end);
